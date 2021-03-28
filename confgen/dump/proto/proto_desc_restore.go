24096bd705bd84caa4ff57f31653e5e99e6314aa
xinnjiege <xinnjiege@tencent.com>
2021-03-28 22:11:45 +0800

    add dump test

---
+++ confgen/dump/proto/proto_desc_restore.go
@@ -0,0 +1,83 @@
+package proto
+
+import (
+	"fmt"
+	"google.golang.org/protobuf/types/descriptorpb"
+	"io"
+)
+
+func Restore(w io.Writer, proto *descriptorpb.FileDescriptorProto) {
+	fmt.Fprintf(w, "syntax = \"%s\";\n\n"+
+		"package %s;\n\n", proto.GetSyntax(), proto.GetPackage())
+
+	for _, message := range proto.MessageType {
+		RestoreFromMessage(w, message)
+	}
+}
+
+func RestoreFromMessage(w io.Writer, msg *descriptorpb.DescriptorProto) {
+	fmt.Fprintf(w, "message %s {\n", *msg.Name)
+	for _, field := range msg.Field {
+		RestoreFromField(w, field)
+	}
+	fmt.Fprintf(w, "}")
+}
+
+func RestoreFromField(w io.Writer, field *descriptorpb.FieldDescriptorProto) {
+	fmt.Fprintf(w, "  %s%s %s = %d;\n", labelString(field.GetLabel()), typeString(field), *field.Name, *field.Number)
+}
+
+func typeString(field *descriptorpb.FieldDescriptorProto) string {
+	switch field.GetType() {
+	case descriptorpb.FieldDescriptorProto_TYPE_DOUBLE:
+		return "double"
+	case descriptorpb.FieldDescriptorProto_TYPE_FLOAT:
+		return "float"
+	case descriptorpb.FieldDescriptorProto_TYPE_INT64:
+		return "int64"
+	case descriptorpb.FieldDescriptorProto_TYPE_UINT64:
+		return "uint64"
+	case descriptorpb.FieldDescriptorProto_TYPE_INT32:
+		return "int32"
+	case descriptorpb.FieldDescriptorProto_TYPE_FIXED64:
+		return "fixed64"
+	case descriptorpb.FieldDescriptorProto_TYPE_FIXED32:
+		return "fixed32"
+	case descriptorpb.FieldDescriptorProto_TYPE_BOOL:
+		return "bool"
+	case descriptorpb.FieldDescriptorProto_TYPE_STRING:
+		return "string"
+	case descriptorpb.FieldDescriptorProto_TYPE_BYTES:
+		return "bytes"
+	case descriptorpb.FieldDescriptorProto_TYPE_UINT32:
+		return "uint32"
+	case descriptorpb.FieldDescriptorProto_TYPE_SFIXED32:
+		return "sfixed32"
+	case descriptorpb.FieldDescriptorProto_TYPE_SFIXED64:
+		return "sfixed64"
+	case descriptorpb.FieldDescriptorProto_TYPE_SINT32:
+		return "sint32"
+	case descriptorpb.FieldDescriptorProto_TYPE_SINT64:
+		return "sint64"
+	case descriptorpb.FieldDescriptorProto_TYPE_ENUM:
+		fallthrough
+	case descriptorpb.FieldDescriptorProto_TYPE_MESSAGE:
+		return field.GetTypeName()
+	case descriptorpb.FieldDescriptorProto_TYPE_GROUP:
+		panic("not implemented")
+	}
+	panic("not implemented")
+}
+
+func labelString(label descriptorpb.FieldDescriptorProto_Label) string {
+	switch label {
+	case descriptorpb.FieldDescriptorProto_LABEL_OPTIONAL:
+		// TODO 按照 syntax 选择是否打印 optional
+		return ""
+	case descriptorpb.FieldDescriptorProto_LABEL_REQUIRED:
+		return "required "
+	case descriptorpb.FieldDescriptorProto_LABEL_REPEATED:
+		return "repeated "
+	}
+	panic("invalid label")
+}
---
+++ confgen/dump/proto/proto_desc_restore_test.go
@@ -0,0 +1,86 @@
+package proto
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/protobuf/types/descriptorpb"
+)
+
+func TestRestore(t *testing.T) {
+	var (
+		syntax      = "proto3"
+		packageName = "example.foo"
+		messageName = "Foo"
+
+		labelOptional = descriptorpb.FieldDescriptorProto_LABEL_OPTIONAL
+		labelRepeated = descriptorpb.FieldDescriptorProto_LABEL_REPEATED
+
+		uint32fieldName         = "foo_uint32"
+		uint32fieldNumber int32 = 1
+		uint32Type              = descriptorpb.FieldDescriptorProto_TYPE_UINT32
+
+		int32fieldName         = "foo_int32"
+		int32fieldNumber int32 = 2
+		int32Type              = descriptorpb.FieldDescriptorProto_TYPE_INT32
+
+		uint64fieldName         = "foo_uint64"
+		uint64fieldNumber int32 = 3
+		uint64Type              = descriptorpb.FieldDescriptorProto_TYPE_UINT64
+
+		int64fieldName         = "foo_int64"
+		int64fieldNumber int32 = 4
+		int64Type              = descriptorpb.FieldDescriptorProto_TYPE_INT64
+
+		repeatedStringFieldName         = "foo_list_string"
+		repeatedStringFieldNumber int32 = 5
+		stringType                      = descriptorpb.FieldDescriptorProto_TYPE_STRING
+
+		proto = &descriptorpb.FileDescriptorProto{
+			MessageType: []*descriptorpb.DescriptorProto{
+				{
+					Name: &messageName,
+					Field: []*descriptorpb.FieldDescriptorProto{
+						{
+							Name:   &uint32fieldName,
+							Number: &uint32fieldNumber,
+							Label:  &labelOptional,
+							Type:   &uint32Type,
+						},
+						{
+							Name:   &int32fieldName,
+							Number: &int32fieldNumber,
+							Label:  &labelOptional,
+							Type:   &int32Type,
+						},
+						{
+							Name:   &uint64fieldName,
+							Number: &uint64fieldNumber,
+							Label:  &labelOptional,
+							Type:   &uint64Type,
+						},
+						{
+							Name:   &int64fieldName,
+							Number: &int64fieldNumber,
+							Label:  &labelOptional,
+							Type:   &int64Type,
+						},
+						{
+							Name:   &repeatedStringFieldName,
+							Number: &repeatedStringFieldNumber,
+							Label:  &labelRepeated,
+							Type:   &stringType,
+						},
+					},
+				},
+			},
+			Package: &packageName,
+			Syntax:  &syntax,
+		}
+	)
+
+	w := &bytes.Buffer{}
+	Restore(w, proto)
+	assert.EqualValues(t, "syntax = \"proto3\";\n\npackage example.foo;\n\nmessage Foo {\n  uint32 foo_uint32 = 1;\n  int32 foo_int32 = 2;\n  uint64 foo_uint64 = 3;\n  int64 foo_int64 = 4;\n  repeated string foo_list_string = 5;\n}", w.String())
+}
